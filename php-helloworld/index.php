060e8b96792d4ff0320a2ead706a8ee056cc1018
valeksic21 <vjekoslav.aleksic@gmail.com>
2022-08-29 22:33:22 +0200

    correction

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,5 +2,5 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 <print "<br>New line\n";
 print "<br>Greetings!\n";
-print "<br>Today is " . date("Y/m/d") 
+print "<br>Today is " . date("Y/m/d"); 
 ?>
