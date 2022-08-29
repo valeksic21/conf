66c568e3c7d486fe0232b10845b12239975753b4
valeksic21 <vjekoslav.aleksic@gmail.com>
2022-08-29 22:21:00 +0200

    init

---
+++ php-helloworld/index_final.php
@@ -0,0 +1,7 @@
+<?php
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "<br>New line\n";
+print "<br>Greetings!\n";
+print "<br>Today is " . date("Y/m/d"); 
+print "<br>Additional message: " . getenv('APP_MSG');
+?>
