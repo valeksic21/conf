c489d180a419e458e2df8d70976c661de2adba2d
valeksic21 <vjekoslav.aleksic@gmail.com>
2022-08-29 23:29:40 +0200

    change

--- php-helloworld/index_final.php
+++ php-helloworld/index_final.php
@@ -1,6 +1,6 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "<br>New line\n";
+print "<br>New new line\n";
 print "<br>Greetings!\n";
 print "<br>Today is " . date("Y/m/d"); 
 print "<br>Additional message: " . getenv('APP_MSG');
