f85f2342806cc3d1604e2d63fb6178c29526ba43
valeksic21 <vjekoslav.aleksic@gmail.com>
2022-08-30 15:02:32 +0200

    new line added

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,7 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "<br>New line\n";
-print "<br>Greetings!!\n";
+print "<br>Greetings!\n";
 print "<br>Today is " . date("Y/m/d"); 
+print "<br>This presentation is disaster!\n";
 ?>
